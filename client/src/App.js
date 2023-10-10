8d2f347c10745ed37cb6f1c4ffa58165d723cfcf
Rishad Alam <rishad.s.a@gmail.com>
2023-10-10 12:24:15 -0400

    joinroom - bugfix

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import { useState } from 'react';
-import io from 'socket.io-client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import "./App.css";
+import { useState } from "react";
+import io from "socket.io-client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
-const socket = io('http://localhost:4000');
+const socket = io("http://localhost:4000");
 
 function App() {
-
   //States
-const [username, setUsername] = useState('');
-const [room, setRoom] = useState('');
-// const [showChat, setShowChat] = useState(false);
-
-
-
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  // const [showChat, setShowChat] = useState(false);
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/' element={<Home
-          username={username} />} />
-          setUsername={setUsername}
-          room={room}
-          setRoom={setRoom}
-          socket={socket}
+          <Route
+            path="/"
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
