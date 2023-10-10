3e71f4a77667e460d02be50139e73f62a5298a0f
Rishad Alam <rishad.s.a@gmail.com>
2023-10-10 11:55:34 -0400

    join room - implement - form, css, states and socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import io from 'socket.io-client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+
+const socket = io('http://localhost:4000');
 
 function App() {
+
+  //States
+const [username, setUsername] = useState('');
+const [room, setRoom] = useState('');
+// const [showChat, setShowChat] = useState(false);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<Home
+          username={username} />} />
+          setUsername={setUsername}
+          room={room}
+          setRoom={setRoom}
+          socket={socket}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
